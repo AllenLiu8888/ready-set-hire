e23f288210559fa091bcad4bd4c766f156d52518
AllenLiu8888 <153785526+AllenLiu8888@users.noreply.github.com>
2025-09-22 21:02:22 +1000

    Set up initial React Router scaffold

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import Layout from './components/Layout'
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+const DashboardPage = () => <div className="p-6">Dashboard</div>
+const InterviewsPage = () => <div className="p-6">Interviews</div>
+const QuestionsPage = () => <div className="p-6">Questions</div>
+const ApplicantsPage = () => <div className="p-6">Applicants</div>
+const TakeInterviewPage = () => <div className="p-6">Take Interview</div>
+const NotFoundPage = () => <div className="p-6">Page Not Found</div>
 
 function App() {
-
   return (
-
+    <Routes>
+      <Route path="/" element={<DashboardPage />} />
+      <Route path="/interviews" element={<InterviewsPage />} />
+      <Route path="/questions" element={<QuestionsPage />} />
+      <Route path="/applicants" element={<ApplicantsPage />} />
+      <Route path="/take/:applicantId" element={<TakeInterviewPage />} />
+      <Route path="/home" element={<Navigate to="/" replace />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   )
 }
 
